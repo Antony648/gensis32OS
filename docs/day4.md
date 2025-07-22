e854e0eb76046028c77a136654a774832b50c748
antony <antonyjoseph0700@gmail.com>
2025-07-22 23:19:54 +0530

    created make file,linker script and bash script

---
+++ docs/day4.md
@@ -0,0 +1,15 @@
+Day4- CREATE MAKEFILE,BASH_SCRIPT AND LINKER SCRIPT
+
+WORKDONE:
+    created a folder called final for storing os.bin
+    created linker file for for kernel.bin file 
+    created makefile for 
+    split the 32 bit kernel part from bootloader and created as a separate kernel.bin file
+    analysed the final os.bin
+NOTES:
+    [explanation and neccesity of linker script](../notes/linker_notes)
+    [indepth explanation of Makefile](../notes/make_file)
+    [explanation of bash script](../notes/bash_script_exp)
+
+TOOLS:
+    GNU make, vim, hexdump 
--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -1,43 +1,17 @@
-org	0x7c00
-bits	16
-
-start:
-	jmp	main
-puts:
-	push 	si
-	push 	ax
-.loop:
-	lodsb	;increments si and loads value at si to al
-	test 	al,al
-	jz	.done
-	;parameters for bios char print
-	;ah=0x0e
-	;bh=0x00
-
-	mov 	ah,0x0e
-	mov	bh,0x00
-	int	0x10
-	jmp 	.loop
-.done:
-	pop 	ax
-	pop 	si
-	ret
-
-msg:	db 'hello form antosbootloader',0
-
-main:
-	mov 	ax,0
-	mov 	ss,ax
-	mov	ds,ax
+[BITS 32]
+global _start
+_start:
+	mov	ax,0x10
+	mov	ss,ax
 	mov	es,ax
-	mov 	sp,0x7c00
-	
-	mov	si,msg
-	call	puts
-	hlt
-.halt:
-	jmp	.halt
+	mov	gs,ax
+	mov	fs,ax
+	mov 	ds,ax
+	mov 	ebp,0x200000
+	mov 	esp,ebp
 
-times 510-($-$$) db 0
-dw 0xaa55
-	
+	in	al,0x92
+	or	al,0x02
+	out	0x92,al
+
+	jmp $
