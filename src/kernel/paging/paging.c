943b5026c40c02445f316baa5bd3fe9c1970acc4
antony <antonyjoseph0700@gmail.com>
2025-08-06 21:54:39 +0530

    some bug fixes for paging

--- src/kernel/paging/paging.c
+++ src/kernel/paging/paging.c
@@ -1,11 +1,11 @@
 #include "paging.h"
 #include "../heap/heap.h"
-dir_table_address create_32_dir_table(uint8_t flags)
+dir_table_address create_32_dir_table(uint32_t flags)
 {
 	//this function creates a directory table by allocating space(4kb) for itself
 	//then allocate 4kb upto 1024 times , so 4mb addtionally, and move the returned addresses
 	//to the corresponding entries to page directory table,
-	dir_table_address rtn_val= kzalloc(NO_DIR_ENTRIES*sizeof(dir_table_entries));
+	dir_table_address rtn_val= (dir_table_address)kzalloc(NO_DIR_ENTRIES*sizeof(dir_table_entries));
 	dir_table_entries index_val=0x00;
 	dir_table_address index=0x00;
 	for(int i=0;i<NO_DIR_ENTRIES;i++)
@@ -14,9 +14,9 @@ dir_table_address create_32_dir_table(uint8_t flags)
 		index_val=(dir_table_entries)kzalloc(NO_PAGE_ENTRIES*sizeof(page_table_entries)); 
 		//kzalloc desingned to return 4kb aligned addresses
 		//as address returned is a mulitple of 4096
-		//last 12 bits are aldready zero
-		index_val |= flags;
-		index*=index_val;
+		//last 12 bits are aldready zero but quick asserting is cheap and helps in later debugging
+		
+		*index=((uint32_t)index_val & 0xfffff000)| (flags& 0xfff);;
 		
 		//code for setting flags
 		
--- src/kernel/paging/paging.h
+++ src/kernel/paging/paging.h
@@ -32,11 +32,11 @@
 //lower 5 bits have the same purpose for bit ,so we can share bit maps
 typedef uint32_t dir_table_entries;
 //if things go right it should be a 32 bit value,
-typedef struct dir_table_entries* dir_table_address ;
+typedef  dir_table_entries* dir_table_address ;
 
 typedef uint32_t 	page_table_entries;
 
-typedef struct page_table_entries* page_table_address;
+typedef  page_table_entries* page_table_address;
 #define PAGING_PRESENT		0b00000001
 #define READ_AND_WRITE		0b00000010
 #define ACCESS_ALL 			0b00000100
