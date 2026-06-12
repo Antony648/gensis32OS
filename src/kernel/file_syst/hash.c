099b09ba55a0d9bc492432306ab43d8e24ab85d7
antony <antonyjoseph0700@gmail.com>
2026-06-12 17:00:28 +0530

    added functions to hash files

--- src/kernel/file_syst/hash.c
+++ src/kernel/file_syst/hash.c
@@ -1,5 +1,12 @@
 #include "hash.h"
+#include <stdint.h>
 uint64_t generate_hash(char* path)
 {
-    return NULL;
+    //destroy the last / donot include in path, and exclude null char at string termination
+    return 0;
+}
+uint64_t generate_hast_start_end(char *str, uint32_t start, uint32_t end)
+{
+    //inclusive ; include start and end
+    return 0;
 }
--- src/kernel/file_syst/hash.h
+++ src/kernel/file_syst/hash.h
@@ -2,4 +2,5 @@
 #define HASH_H
 #include <stdint.h>
 uint64_t generate_hash(char* str);
+uint64_t generate_hast_start_end(char* ,uint32_t ,uint32_t);
 #endif
