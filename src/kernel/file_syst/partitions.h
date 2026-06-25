ebf1b6d3562de40d0de72c83a3af7be4f88b2cdf
antony <antonyjoseph0700@gmail.com>
2026-06-25 17:50:20 +0530

    moved partitions definition to disk.c due to conflict

--- src/kernel/file_syst/partitions.h
+++ src/kernel/file_syst/partitions.h
@@ -2,15 +2,6 @@
 #define PARTITIONS_H
 #include "../disk/disk.h"
 #include <stdint.h>
-struct partition
-{
-	struct disk* f_disk;
-	enum FILE_SYST_TYPE fs_type;
-	uint32_t start_sect;
-	uint32_t sect_num;
-	uint8_t is_bootable;
-	struct partition* next;
-};
 void single_disk_scan(struct disk* disk_1);
 void scan_part_all_disks();
 void partition_debug();
