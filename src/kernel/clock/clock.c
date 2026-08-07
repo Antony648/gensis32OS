f232162449237021dd35c330f437c1b268916c6b
antony <antonyjoseph0700@gmail.com>
2026-08-07 16:35:54 +0530

    modified functions dfined and decclared in clock.c and clock.h files

--- src/kernel/clock/clock.c
+++ src/kernel/clock/clock.c
@@ -1,6 +1,12 @@
 #include "clock.h"
-uint16_t get_timedate_fat16()
+uint16_t get_time_fat16()
 {
 	return 0;
 }
+uint16_t get_date_fat16()
+{
+	return 0;
+}
+
+
 
--- src/kernel/clock/clock.h
+++ src/kernel/clock/clock.h
@@ -1,5 +1,6 @@
 #ifndef CLOCK_H
 #define CLOCK_H
 #include <stdint.h>
-uint16_t get_timedate_fat16();
+uint16_t get_time_fat16();
+uint16_t get_date_fat16();
 #endif
